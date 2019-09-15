9d6dc703428a8042bec3944fc64d827ed6669963
vered123 <53009675+vered123@users.noreply.github.com>
2019-09-15 22:25:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,8 @@
-const express = require ('express');
-const app = express ();
-const fs = require('fs');
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => {
-  try {
-      fs.readFile('./index.html','utf8', (err,data) =>{
-  if (err) throw err
+// define routes here..
 
-    res.send(`${data}`)
-}) 
-
-} catch(err){
-    console.log(err)
-}
-
-
-})
-
-
-
-app.get('/foo', (req, res) => {
-    res.send('<h1>hi there foo</h1')
-})
-
-
-
-let port = process.env.PORT || 3000;
-
-app.listen(port, function () {
-    console.log('server listen on port', port)
-})
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
